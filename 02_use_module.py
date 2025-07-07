18d177a508a3f11b5e4055afa31b77534a30695b
andy1357910 <zxcb1234567892@gmail.com>
2025-07-07 14:59:58 +0800

    完成模組練習

---
+++ 02_use_module.py
@@ -0,0 +1,11 @@
+# use_module.py
+
+import mymodule
+
+x = 10
+y = 5
+
+print(f"{x} + {y} = {mymodule.add(x, y)}")
+print(f"{x} - {y} = {mymodule.subtract(x, y)}")
+print(f"{x} * {y} = {mymodule.multiply(x, y)}")
+print(f"{x} / {y} = {mymodule.divide(x, y)}")
